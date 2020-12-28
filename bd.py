34929abaae28f0f9b70be4afab06703be3f61d88
lpmole89 <lpmole89@gmail.com>
2020-12-27 21:16:56 -0300

    PEP8 y demas

--- bd.py
+++ bd.py
@@ -1,4 +1,5 @@
 import sqlite3
+import re
 
 #Funcion que crea el backend, DB  y table
 def creardb():
@@ -15,10 +16,15 @@ def creardb():
             print("La tabla libros ya existe")                    
         conexion.close()
 
-def alta(titulo,autor,genero):
+
+def alta(
+        titulo,
+        autor,
+        genero):
     conexion = sqlite3.connect("biblioteca.db")
     print(titulo)
-    conexion.execute("insert into libros(titulo,autor,genero) values (?,?,?)", [titulo, autor, genero])
+    conexion.execute("insert into libros(titulo,autor,genero) \
+                     values (?,?,?)", [titulo, autor, genero])
     conexion.commit()
     conexion.close()
 
@@ -28,18 +34,29 @@ def cargarlista():
     biblioteca = conexion.execute('SELECT * FROM libros')
     return biblioteca
 
-def baja(id):
+
+def baja(
+        id):
     conexion = sqlite3.connect('biblioteca.db')
     conexion.execute('DELETE FROM libros WHERE id = ?', [id])
     conexion.commit()
     conexion.close()
 
 
-def modificar(ide, titulo, autor, genero):
+def modificar(
+        ide, 
+        titulo, 
+        autor, 
+        genero):
     conexion = sqlite3.connect('biblioteca.db')
-    sentencia = "UPDATE libros SET titulo = '" + str(titulo) + "' , autor = '" + str(autor) + "' , genero = '" + str(genero) + "' WHERE id = '" + str(ide) + "'"
-    print(sentencia)
-    conexion.execute(sentencia)
-    #conexion.execute('UPDATE libros SET titulo = ?, autor = ? , genero = ? WHERE id = ?', [titulo, autor, genero, ide])
+    conexion.execute('UPDATE libros SET titulo = ?, autor = ? , genero = ? \
+                     WHERE id = ?', [titulo, autor, genero, ide])
     conexion.commit()
     conexion.close()
+
+
+def validartitulo(
+        titulo):
+    patron = re.compile("^[A-Za-z]+(?:[ _-][A-Za-z]+)*$")
+    validacion = re.match(patron, titulo)
+    return validacion
--- main.py
+++ main.py
@@ -1,54 +1,93 @@
 from tkinter import *
 import tkinter as tk
 import tkinter.ttk as ttk
+from tkinter.messagebox import *
 
 from bd import *
 
-#con comentario
-def refrescarlista():
+
+def refrescar_lista():
     for i in my_tree.get_children(): 
         my_tree.delete(i) 
     biblioteca = cargarlista()  
     for libro in biblioteca:
-        my_tree.insert(parent='', index='end', iid=libro[0], text=" ", values=(libro[0], libro[1], libro[2], libro[3]))
+        my_tree.insert(
+            parent='', 
+            index='end', 
+            iid=libro[0], 
+            text=" ", 
+            values=(libro[0], libro[1], libro[2], libro[3]))
 
-def daralta():
-    alta(ctitulo.get(),cautor.get(),cgenero.get())
-    refrescarlista()
-    limpiar()
-    
+
+def dar_alta():
+    if validartitulo(ctitulo.get()):   
+        alta(ctitulo.get(),cautor.get(),cgenero.get())
+        refrescar_lista()
+        limpiar()
+    else:
+        showerror("Error","Se debe iniciar y terminar con letra o palabra. \
+                  \nLas letras o palabras se pueden separar por un espacio, \
+                  un - o un _.  ") 
+
+
 def limpiar():
-    ctitulo.delete(0, 'end')
-    cautor.delete(0, 'end')
-    cgenero.delete(0, 'end')
+    ctitulo.delete(
+        0, 
+        'end')
+    cautor.delete(
+        0, 
+        'end')
+    cgenero.delete(
+        0, 
+        'end')
 
-def darbaja():
+
+def dar_baja():
     item = my_tree.focus()
     baja(item)
-    refrescarlista()
+    refrescar_lista()
     limpiar()
 
 
-def darmodificar():
+def dar_modificar():
     item = my_tree.focus()
     datos = my_tree.item(item)
     modificar(datos['values'][0],ctitulo.get(),cautor.get(),cgenero.get())
-    refrescarlista()
+    refrescar_lista()
     limpiar()
   
 
-
-def agregarinput(valor,ancho,fila,columna):
-    objentry = Entry(root, width= ancho)
-    objentry.grid(row=fila, column=columna, columnspan=2) 
+def agregar_input(
+        valor,
+        ancho,
+        fila,
+        columna):
+    objentry = Entry(
+        root, 
+        width=ancho)
+    objentry.grid(
+        row=fila, 
+        column=columna, 
+        columnspan=2) 
     objentry.insert(0,valor)
     return objentry
 
-def agregarlabel(valor,fila,columna):
-    objlabel = Label(root, text=valor).grid(row=fila, column=columna,sticky=W,padx=50,columnspan=2)
+
+def agregar_label(
+        valor,
+        fila,
+        columna):
+    objlabel = Label(root, text=valor).grid(
+        row=fila, 
+        column=columna,
+        sticky=W,
+        padx=50,
+        columnspan=2)
     return objlabel
 
-def cargaritem(*args):
+
+def cargar_item(
+        *args):
     item = my_tree.focus()
     datos = my_tree.item(item)
     ctitulo.delete(0, 'end')
@@ -63,49 +102,145 @@ creardb()
 
 root = Tk()
 root.title('Biblioteca')
-root.geometry("1000x500")
+root.geometry("776x300")
 
-Label(root, text="Biblioteca", bg='green', foreground='white').grid(row=0, sticky=W+S+N+E, columnspan=10)
-root.grid_columnconfigure(0, pad=50)
-agregarlabel("Titulo",1,0)
-agregarlabel("Autor",3,0)
-agregarlabel("Genero",5,0)
-ctitulo = agregarinput('',30,2,0)
-cautor = agregarinput('',30,4,0)
-cgenero = agregarinput('',30,6,0)
+Label(root, text="Biblioteca", bg='green', foreground='white').grid(
+    row=0, 
+    sticky=W+S+N+E, 
+    columnspan=10
+    )
+root.grid_columnconfigure(0, pad = 50)
+agregar_label("Titulo", 1, 0)
+agregar_label("Autor", 3, 0)
+agregar_label("Genero", 5, 0)
+ctitulo = agregar_input('', 30, 2, 0)
+cautor = agregar_input('', 30, 4, 0)
+cgenero = agregar_input('', 30, 6, 0)
 
 
-balta = Button(root, text="Alta", padx=5, width=12, command=daralta)
-bbaja = Button(root, text="Baja", padx=5, width=12, command=darbaja) 
-bmodificar = Button(root, text="Modificar", padx=5, width=12, command=darmodificar) 
-blimpiar = Button(root, text="Limpiar", padx=5, width=12, command=limpiar) 
+balta = Button(
+    root, 
+    text="Alta", 
+    padx=5, 
+    width=12, 
+    command=dar_alta
+    )
+bbaja = Button(
+    root, 
+    text="Baja", 
+    padx=5, 
+    width=12, 
+    command=dar_baja
+    ) 
+bmodificar = Button(
+    root, 
+    text="Modificar", 
+    padx=5, 
+    width=12, 
+    command=dar_modificar
+    ) 
+blimpiar = Button(
+    root, 
+    text="Limpiar", 
+    padx=5, 
+    width=12, 
+    command=limpiar
+    ) 
 
-balta.grid(row=8, column=0)
-bbaja.grid(row=8, column=1)
-bmodificar.grid(row=9, column=0)
-blimpiar.grid(row=9, column=1)
+balta.grid(
+    row=8, 
+    column=0
+    )
+bbaja.grid(
+    row=8, 
+    column=1
+    )
+bmodificar.grid(
+    row=9, 
+    column=0
+    )
+blimpiar.grid(
+    row=9, 
+    column=1
+    )
 
 my_tree = ttk.Treeview(root)
-my_tree.grid(row=1, column=4, rowspan=10, pady=20, padx=20)
-my_tree['columns'] = ('ID', 'Titulo', 'Autor', 'Genero')
+my_tree.grid(
+    row=1, 
+    column=4, 
+    rowspan=10, 
+    pady=20, 
+    padx=20
+    )
+my_tree['columns'] = (
+    'ID', 
+    'Titulo', 
+    'Autor', 
+    'Genero'
+    )
 
 #CreateColumns
-my_tree.column('#0', width=0, minwidth=25)
-my_tree.column('ID', anchor=W, width=120)
-my_tree.column('Titulo', anchor=W, width=120)
-my_tree.column('Autor', anchor=W, width=120)
-my_tree.column('Genero', anchor=W, width=120)
+my_tree.column(
+    '#0', 
+    width=0, 
+    minwidth=25
+    )
+my_tree.column(
+    'ID', 
+    anchor=W, 
+    width=120
+    )
+my_tree.column(
+    'Titulo', 
+    anchor=W, 
+    width=120
+    )
+my_tree.column(
+    'Autor', 
+    anchor=W, 
+    width=120
+    )
+my_tree.column(
+    'Genero', 
+    anchor=W, 
+    width=120
+    )
 
 #CreateHeading
-my_tree.heading('#0', text='Label', anchor=W)
-my_tree.heading('ID', text='ID', anchor=W)
-my_tree.heading('Titulo', text='Tirulo', anchor=W)
-my_tree.heading('Autor', text='Autor', anchor=W)
-my_tree.heading('Genero', text='Genero', anchor=W)
+my_tree.heading(
+    '#0', 
+    text=' ', 
+    anchor=W
+    )
+my_tree.heading(
+    'ID', 
+    text='ID', 
+    anchor=W
+    )
+my_tree.heading(
+    'Titulo', 
+    text='Tirulo', 
+    anchor=W
+    )
+my_tree.heading(
+    'Autor', 
+    text='Autor', 
+    anchor=W
+    )
+my_tree.heading(
+    'Genero', 
+    text='Genero', 
+    anchor=W
+    )
 
-my_tree.bind('<ButtonRelease-1>', cargaritem)
+my_tree.bind(
+    '<ButtonRelease-1>', 
+    cargar_item
+    )
 
-refrescarlista()
+refrescar_lista()
 
 my_tree.pack
 mainloop()
+
+
