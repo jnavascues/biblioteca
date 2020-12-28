8d639bfcb38954a7e2c3b688d36fec826b2fec78
jnavascues <jnavascues@jamcity.com>
2020-12-28 02:27:00 -0300

    Added docstring a la clase validacion

--- validador.py
+++ validador.py
@@ -2,21 +2,33 @@ import re
 
 
 class validacion:
+    """
+    Clase para validaciones de los campos
+    """
     def __init__(self, ):
         pass
     
 
     def validar_titulo(self, titulo):
+        """
+        Metodo de validacion de titulo
+        """
         patron = re.compile("^[A-Za-z]+(?:[ _-][A-Za-z]+)*$")
         validacion = re.match(patron, titulo)
         return validacion
 
     def validar_autor(self, autor):
+        """
+        Metodo de validacion de Autor
+        """
         patron = re.compile("^[A-Za-z]+(?:[ _-][A-Za-z]+)*$")
         validacion = re.match(patron, autor)
         return validacion
 
     def validar_genero(self, genero):
+        """
+        Metodo de validacion de genero
+        """
         patron = re.compile("^[A-Za-z]+(?:[ _-][A-Za-z]+)*$")
         validacion = re.match(patron, genero)
         return validacion
